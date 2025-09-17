a65c9b15aebfaefad03caafbdd0ea791f24eced3
ridiccur <heafraer@gmail.com>
2025-09-17 17:34:43 +0300

    Add PostgreSQL dependencies and configure JPA settings; implement product retrieval by ID

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,15 @@ public class MainController {
     @GetMapping("/products")
     private List<Product> getProducts(){
         return productService.getAll();
+    }
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        for (Product product : productService.getAll()) {
+            if (product.getId().equals(id)) {
+                return ResponseEntity.ok(product);
+            } 
+        }
+        return ResponseEntity.notFound().build();
     }
     @PostMapping("/products")
     private ResponseEntity<Product> addProduct(@RequestBody @Valid Product product) {
--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,17 +1,34 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
     @NotBlank(message = "Название пустое")
+    @Size(min = 2, max = 100, message = "Название должно быть от 2 до 100 символов")
     private String title;
-    @Min(100)
+
+    @Column(nullable = false)
+    @Min(1)
     private int cost;
 }
